261a7a1632af0bd006f0d068737283b36844ca14
Max Rigout <71095568+maxrigout@users.noreply.github.com>
2023-06-11 19:43:46 -0400

    refactor

--- index.js
+++ index.js
@@ -1,320 +1,43 @@
 const fileSelectedClass = "file-selected";
+const folderIconPath = "icons/folder.png";
+const parentFolderIconPath = "icons/folder.png";
+const imageIconPath = "icons/file.png";
+const audioIconPath = "icons/file.png";
+const pdfIconPath = "icons/file.png";
+const musescoreIconPath = "icons/file.png";
+const defaultFileIconPath = "icons/file.png";
 
 const dialog = document.querySelector("#myDialog");
 const dialogRoot = dialog.querySelector("#dialog-root");
 const dialogTitleBar = dialog.querySelector(".dialog-title-bar");
 const dialogFileList = dialog.querySelector(".dialog-file-list");
 const dialogFilePreview = dialog.querySelector(".dialog-file-preview");
-const dialogActionButtons = dialog.querySelector("#dialog-action-buttons");
 
 let pathList = [];
 let selectedElement = null;
 let fetchedData;
-const defaultDialogContent = dialogRoot.innerHTML;
 
-const svg = {
-	folder: `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill-rule="evenodd"><path d="M3.908 4h10.104c1.163 0 1.582.073 2.032.229.45.156.838.395 1.179.728.34.333.593.675 1.113 1.716L19 8H0v-.092c0-.866.162-1.547.467-2.117a3.18 3.18 0 0 1 1.324-1.324C2.36 4.162 3.042 4 3.908 4zM0 8h34.872c1.783 0 2.43.186 3.082.534.652.349 1.163.86 1.512 1.512.348.652.534 1.299.534 3.082v17.744c0 1.783-.186 2.43-.534 3.082a3.635 3.635 0 0 1-1.512 1.512c-.652.348-1.299.534-3.082.534H5.128c-1.783 0-2.43-.186-3.082-.534a3.635 3.635 0 0 1-1.512-1.512C.186 33.302 0 32.655 0 30.872V8z"></path></g></svg>`,
-	plus: `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M13 11h6a1 1 0 0 1 0 2h-6v6a1 1 0 0 1-2 0v-6H5a1 1 0 0 1 0-2h6V5a1 1 0 0 1 2 0v6z"></path></svg>`,
-	x: `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17.251 8.157L13.421 12l3.83 3.843a.996.996 0 0 1-1.408 1.408L12 13.421l-3.843 3.83a.996.996 0 0 1-1.408-1.408L10.579 12l-3.83-3.843A.996.996 0 0 1 8.157 6.75L12 10.579l3.843-3.83a.996.996 0 0 1 1.408 1.408z" fill-rule="evenodd"></path></svg>`,
-	search: `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M15.907 17.319a8 8 0 1 1 1.412-1.412c.013.011.026.023.038.036l4.35 4.35a1 1 0 0 1-1.414 1.414l-4.35-4.35a1.016 1.016 0 0 1-.036-.038zM11 17a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>`,
-}
-
-const animalFiles = [
-	{
-		type: "folder",
-		name: "chats"
-	},
-	{
-		type: "folder",
-		name: "oiseaux"
-	},
-		{
-		type: "folder",
-		name: "chiens"
-	}
-];
-
-const catFiles = [
-	{
-		type: "jpeg",
-		name: "cat.jpeg",
-		url: "img/cats/cat.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "catz.jpg",
-		url: "img/cats/catz.jpg",
-	},
-	{
-		type: "jpg",
-		name: "domestic-cat.jpg",
-		url: "img/cats/domestic-cat.jpg",
-	},
-	{
-		type: "jpeg",
-		name: "kittens.jpeg",
-		url: "img/cats/kittens.jpeg",
-	},
-	{
-		type: "jpeg",
-		name: "cats-party.jpeg",
-		url: "img/cats/cats-party.jpeg",
-	},
-	{
-		type: "jpeg",
-		name: "black-cat-back.jpeg",
-		url: "img/cats/black-cat-back.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "cats1.jpg",
-		url: "img/cats/	cats1.jpg",
-	},
-	{
-		type: "jpg",
-		name: "kittens1.jpg",
-		url: "img/cats/kittens1.jpg",
-	},
-	{
-		type: "jpeg",
-		name: "egypt-kitty.jpeg",
-		url: "img/cats/egypt-kitty.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "white-cat-min.jpg",
-		url: "img/cats/white-cat-min.jpg",
-	},
-	{
-		type: "jpg",
-		name: "p07ryyyj.jpg",
-		url: "img/cats/p07ryyyj.jpg",
-	},
-];
-
-const dogFiles = [
-	{
-		type: "png",
-		name: "dog.png",
-		url: "img/dogs/dog.jpeg",
-	},
-	{
-		type: "png",
-		name: "zouzou.png",
-		url: "img/dogs/zouzou.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "puppy-on-garden.jpg",
-		url: "img/dogs/puppy-on-garden.jpg",
-	},
-	{
-		type: "png",
-		name: "angry_chihuahua.png",
-		url: "img/dogs/angry_chihuahua.png",
-	},
-	{
-		type: "jpg",
-		name: "fuffy-dog.jpg",
-		url: "img/dogs/fuffy-dog.jpg",
-	},
-];
-
-const birdFiles = [
-	{
-		type: "jpeg",
-		name: "bird2.jpeg",
-		url: "img/birds/bird2.jpeg",
-	},
-	{
-		type: "jpeg",
-		name: "bird.jpeg",
-		url: "img/birds/bird.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "Tanager-Shapiro.jpg",
-		url: "img/birds/Tanager-Shapiro.jpg",
-	},
-];
-
-const rootFiles = [
-	{
-		type: "folder",
-		name: "pictures"
-	},
-	{
-		type: "folder",
-		name: "animaux"
-	},
-	{
-		type: "jpeg",
-		name: "cat.jpeg",
-		url: "img/cats/cat.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "catz.jpg",
-		url: "img/cats/catz.jpg",
-	},
-	{
-		type: "jpg",
-		name: "domestic-cat.jpg",
-		url: "img/cats/domestic-cat.jpg",
-	},
-	{
-		type: "jpeg",
-		name: "kittens.jpeg",
-		url: "img/cats/kittens.jpeg",
-	},
-	{
-		type: "jpeg",
-		name: "cats-party.jpeg",
-		url: "img/cats/cats-party.jpeg",
-	},
-	{
-		type: "jpeg",
-		name: "black-cat-back.jpeg",
-		url: "img/cats/black-cat-back.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "cats1.jpg",
-		url: "img/cats/	cats1.jpg",
-	},
-	{
-		type: "jpg",
-		name: "kittens1.jpg",
-		url: "img/cats/kittens1.jpg",
-	},
-	{
-		type: "jpeg",
-		name: "egypt-kitty.jpeg",
-		url: "img/cats/egypt-kitty.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "white-cat-min.jpg",
-		url: "img/cats/white-cat-min.jpg",
-	},
-	{
-		type: "jpg",
-		name: "p07ryyyj.jpg",
-		url: "img/cats/p07ryyyj.jpg",
-	},
-	{
-		type: "png",
-		name: "dog.png",
-		url: "img/dogs/dog.jpeg",
-	},
-	{
-		type: "png",
-		name: "zouzou.png",
-		url: "img/dogs/zouzou.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "puppy-on-garden.jpg",
-		url: "img/dogs/puppy-on-garden.jpg",
-	},
-	{
-		type: "png",
-		name: "angry_chihuahua.png",
-		url: "img/dogs/angry_chihuahua.png",
-	},
-	{
-		type: "jpg",
-		name: "fuffy-dog.jpg",
-		url: "img/dogs/fuffy-dog.jpg",
-	},
-	{
-		type: "jpeg",
-		name: "bird2.jpeg",
-		url: "img/birds/bird2.jpeg",
-	},
-	{
-		type: "jpeg",
-		name: "bird.jpeg",
-		url: "img/birds/bird.jpeg",
-	},
-	{
-		type: "jpg",
-		name: "Tanager-Shapiro.jpg",
-		url: "img/birds/Tanager-Shapiro.jpg",
-	},
-]
-
-const pictureFiles = [
-	{
-		type: "folder",
-		name: "goesnowhere"
-	},
-	{
-		type: "png",
-		name: "cat.png",
-		url: "img/cat.jpeg",
-	},
-	{
-		type: "png",
-		name: "bird.png",
-		url: "img/birds/bird.jpeg",
-	},
-	{
-		type: "png",
-		name: "bird.png",
-		url: "img/birds/bird.jpeg",
-	},
-]
-
-const fetchDocs_hardCoded = async (url) => {
-	let data = null;
-	if (url === "root") {
-		data = rootFiles
-	} else if (url === "root/pictures") {
-		data = pictureFiles;
-	} else if (url === "root/pictures/folder1") {
-		data = folder1Files;
-	} else if (url === "root/animaux") {
-		data = animalFiles;
-	} else if (url === "root/animaux/chats") {
-		data = catFiles;
-	} else if (url === "root/animaux/chiens") {
-		data = dogFiles;
-	} else if (url === "root/animaux/oiseaux") {
-		data = birdFiles;
-	}
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (data) {
-				resolve(data);
-			}
-			else {
-				reject({error: "no data available"});
-			}
-		}, 500)
-	});
-}
-
-const fetchDocs_server = async (path) => {
+const fetchDocs = async (path) => {
 	const baseUrl = "http://192.168.1.222:3000/getliste.php";
 	const myHeaders = new Headers();
-	myHeaders.set("Content-Type", "application/json");
+
+	// for a JSON body...
+	// const body = JSON.stringify({path: path}).toString();
+	// myHeaders.set("Content-Type", "application/json");
+
+	// for a form encoded body...
+	const body = new URLSearchParams();
+	body.append("path", path);
 	const myRequest = new Request(baseUrl, {
 		method: "POST",
 		headers: myHeaders,
-		body: JSON.stringify({path: path}),
+		body: body,
 	});
 
 	const response = await fetch(myRequest);
 	const jsonData = await response.json();
 	return jsonData;
 }
-
-const fetchDocs = async (path) => {
-	// return await fetchDocs_server(path);
-	return await fetchDocs_hardCoded(path);
-}
-
 
 // https://stackoverflow.com/questions/9068156/server-side-file-browsing
 const fileBrowser = (cb) => {
@@ -347,53 +70,40 @@ const fileBrowser = (cb) => {
 	input.click();
 }
 
-const renderTitleBar = () => {
-	return `<div class="vertical-center align-right">
-	${svg.search}
-</div>
-<input placeholder="Search" id="title-bar-search" type="text">
-<button id="title-bar-button-add">
-	${svg.plus}
-</button>
-<button id="title-bar-button-cancel">
-	${svg.x}
-</button>`
-}
-
 const updatePreview = () => {
 	const filePreview = dialogFilePreview.querySelector(".file-preview");
 	if (selectedElement === null) {
-		filePreview.style.visibility = "hidden"
+		filePreview.style.visibility = "hidden";
 		return;
 	}
 	if (selectedElement.data.type === "folder") {
-		filePreview.style.visibility = "hidden"
+		filePreview.style.visibility = "hidden";
 		filePreview.innerHTML = "";
 		return;
 	} else if (selectedElement.data.type === "parentFolder") {
-		filePreview.style.visibility = "hidden"
+		filePreview.style.visibility = "hidden";
 		filePreview.innerHTML = "";
 		return;
 	}
-	filePreview.style.visibility = "visible"
-	filePreview.innerHTML = `<img src="${selectedElement.data.url}">`
+	filePreview.style.visibility = "visible";
+	filePreview.innerHTML = `<img src="${selectedElement.data.url}">`;
 }
 
 const selectElement = (e, i) => {
 	if (selectedElement !== null) {
 		if (selectedElement.index === i) {
-			console.log(`element already selected`);
+			console.debug(`element already selected`);
 		}
 		selectedElement.node.classList.remove(fileSelectedClass);
 	}
-	console.log(`select element ${i}`);
+	console.debug(`select element ${i}`);
 	selectedElement = { 
 		node: e,
 		index: i,
 		data: null,
 	};
 	if (i === -1) {
-		selectedElement.data = {type: "parentFolder"}
+		selectedElement.data = { type: "parentFolder" };
 	} else if (i >= 0 && i < fetchedData.length) {
 		selectedElement.data = fetchedData[i];
 	}
@@ -403,41 +113,58 @@ const selectElement = (e, i) => {
 
 const navigateToFolder = (folder) => {
 	selectedElement = null;
-	console.log(`navigating to folder`, folder);
+	console.info(`navigating to folder`, folder);
 	dialogFileList.innerHTML = "";
 	pathList.push(folder.data.name);
 	renderDialog();
 }
 
 const navigateUp = () => {
+	console.info("navigating up");
 	selectedElement = null;
 	pathList.pop();
 	renderDialog();
 }
 
 // https://www.w3schools.com/howto/howto_css_image_gallery.asp
-const renderSingleImage = (image, index) => {
-	console.log("rendering image", image);
-	return `<div class="element_container" id="element_${index}" onclick="selectElement(this, ${index});">
-	<img src="icons/file.png">
-		${image.name}
-</div>`;
-}
-
 const renderSingleFile = (file, index) => {
+	let iconPath = defaultFileIconPath;
 	switch (file.type) {
+		case "image":
 		case "jpeg":
 		case "jpg":
-		case "png": return renderSingleImage(file, index);
+		case "png":
+		case "gif":
+		case "bmp":
+			console.debug("rendering image", file);
+			iconPath = imageIconPath;
+			break;
+		case "audio":
+		case "mp3":
+			console.debug("rendering audio file", file);
+			iconPath = audioIconPath;
+			break;
+		case "pdf":
+			console.debug("rendering pdf", file);
+			iconPath = pdfIconPath;
+			break;
+		case "musescore":
+			console.debug("rendering musescore file", file);
+			iconPath = musescoreIconPath;
+			break;
 	}
+	return `<div class="element_container" id="element_${index}" onclick="selectElement(this, ${index});">
+		<img src="${iconPath}">
+		${file.name}
+	</div>`;
 }
 
 const renderSingleFolder = (folder, index) => {
-	console.log("rendering folder", folder, index);
+	console.debug("rendering folder", folder, index);
 	return `<div class="element_container" id="element_${index}" onclick="selectElement(this, ${index});">
-		<img src="icons/folder.png">
+		<img src="${folderIconPath}">
 		${folder.name}
-	</div>`
+	</div>`;
 }
 
 const renderSingleElement = (element, index) => {
@@ -449,31 +176,32 @@ const renderSingleElement = (element, index) => {
 
 const renderParentFolder = () => {
 	return `<div class="element_container" id="element_-1" onclick="selectElement(this, -1);">
-		<img src="icons/folder.png"> dossier parent
+		<img src="${parentFolderIconPath}"> dossier parent
 	</div>`;
 }
 
 const closeDialog = () => {
-	console.log("closing dialog");
+	console.debug("closing dialog");
 	dialog.close();
 }
 
 const closeDialogForSuccess = (cb) => {
-	console.log("closing for success")
+	console.debug("closing for success")
 	cb(selectedElement.data.url, {title: selectedElement.data.name});
 	closeDialog();
 }
 
 const renderContent = (data) => {
 	const renderedContent = data.map((e, index) => renderSingleElement(e, index)).join("");
-	if (pathList.length === 0)
-		return `<div class="file-list">${renderedContent}</div>`
+	if (pathList.length === 0) {
+		return `<div class="file-list">${renderedContent}</div>`;
+	}
 	const renderedParentFolder = renderParentFolder();
-	return `<div class="file-list">${renderedParentFolder}${renderedContent}</div>`
+	return `<div class="file-list">${renderedParentFolder}${renderedContent}</div>`;
 }
 
 const addDialogListeners = (cb) => {
-	console.log("adding dialog listeners");
+	console.debug("adding dialog listeners");
 	const addBtn = dialogTitleBar.querySelector("#title-bar-button-add");
 	const xButton = dialogTitleBar.querySelector("#title-bar-button-cancel");
 	const okButton = dialogRoot.querySelector("#btn-ok");
@@ -482,15 +210,15 @@ const addDialogListeners = (cb) => {
 
 	addBtn.onclick = () => {
 		fileBrowser((data, metadata) => {
-			console.log(data);
-			console.log(metadata);
+			console.debug(data);
+			console.debug(metadata);
 		});
-	}
+	};
 	xButton.onclick = closeDialog;
 	cancelButton.onclick = closeDialog;
 
 	okButton.onclick = () => {
-		console.log(selectedElement);
+		console.debug(selectedElement);
 		if (selectedElement.data.type === "folder")
 			navigateToFolder(selectedElement)
 		else
@@ -498,11 +226,11 @@ const addDialogListeners = (cb) => {
 	};
 
 	elementsContainer.ondblclick = () => {
-		console.log("db click");
+		console.debug("db click");
 		if (selectedElement.data.type === "parentFolder") {
 			navigateUp();
 		} else if (selectedElement.data.type === "folder") {
-			navigateToFolder(selectedElement)
+			navigateToFolder(selectedElement);
 		} else {
 			closeDialogForSuccess(cb);
 		}
@@ -510,7 +238,7 @@ const addDialogListeners = (cb) => {
 }
 
 const renderDialogContent = (data) => {
-	console.log(data);
+	console.debug(data);
 	fetchedData = data;
 	// potentially sort the data
 	// data.sort((e1, e2) => e1.name.localeCompare(e2.name));
@@ -523,21 +251,20 @@ const renderDialogContent = (data) => {
 }
 
 const renderDialog = () => {
-	const path = "root" + (pathList.length === 0 ? "" : "/") + pathList.join("/");
-	console.log(`loading ${path}`);
+	const path = pathList.join("/");
+	console.info(`loading ${path}`);
 	fetchDocs(path)
 		.then(data => {
-			renderDialogContent(data)
+			renderDialogContent(data);
 		})
 		.catch(error => {
-			console.log(error)
+			console.error(error);
 			pathList.pop();
 		});
 }
 
 const filePickerDialogHandler = () => {
 	dialog.showModal();
-	console.log(dialog);
 	renderDialog();
 }
 
@@ -548,7 +275,7 @@ const filePickerHandler = (cb, value, meta) => {
 };
 
 const uploadHandler = (blobInfo, progress) => new Promise((resolve, reject) => {
-	console.log("uploading...")
+	console.log("uploading...");
 
 	const xhr = new XMLHttpRequest();
 	xhr.withCredentials = false;
